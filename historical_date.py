394f8534957d15e0c87121ad14546657703739b4
torishima2000 <ht19a055@oecu.jp>
2021-04-28 16:36:44 +0900

    rename

--- historical_date.py
+++ historical_date.py
@@ -15,7 +15,7 @@ ticker = yf.Ticker("{}.T".format(Security_code))
 hist = ticker.history(period="max")
 
 # データの保存先のファイルへの相対パス
-file_path = os.path.join("historys", "{}.csv".format(Security_code))
+file_path = os.path.join("HistoricalDate", "{}.csv".format(Security_code))
 
 # csvファイルとしてデータを保存
 hist.to_csv(file_path, sep = ",")

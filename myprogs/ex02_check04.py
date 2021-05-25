38f6b11e346f650ba7675fc06ca623d96720ba4e
torishura2000 <ht19a055@oecu,jp>
2021-05-25 12:00:18 +0900

    Make new programs to compare of DataFrame index.

---
+++ myprogs/ex02_check04.py
@@ -0,0 +1,28 @@
+# 取得データの比較2
+
+# 自作プログラム
+# 自作モジュールのインポート
+import mylibrary as mylib
+
+# 損益計算書
+Security_code = "7203.T"
+mylib.pl_to_csv(Security_code)
+my_financials = mylib.get_pl(Security_code).T
+
+
+# サイトのプログラムのコピー
+import yfinance as yf
+
+ticker = yf.Ticker("7203.T")
+financials = ticker.financials
+
+
+# 取得したDataFrameオブジェクトの比較
+import pandas as pd
+import numpy as np
+
+# 欠損値の補完
+my_financials = my_financials.fillna(np.NAN)
+financials = financials.fillna(np.NAN)
+
+print(my_financials.index.dtype == financials.index.dtype)

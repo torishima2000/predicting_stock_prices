a62332083c5549bca2599e2398556f8e12c3c12f
torishima2000 <ht19a055@oecu.jp>
2021-05-25 23:06:12 +0900

    Make programs to check my sorcecodes.

---
+++ myprogs/ex08/ex08_check.py
@@ -0,0 +1,27 @@
+# 取得データの比較8
+
+# 自作プログラム
+# 自作モジュールのインポート
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+import mylibrary as mylib
+
+# 銘柄リストの読み込み
+my_data = mylib.get_codelist_topix500()
+
+
+# サイトのプログラムのコピー
+import datetime
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("topix500.csv")
+
+
+# 取得したオブジェクトの比較
+# オブジェクトのリスト化
+data = data.T.values.tolist()[0]
+print(my_data == data)

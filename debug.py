77bea37288d29d5ef3bec3bf5956ac21f9f30b15
torishura2000 <ht19a055@oecu,jp>
2021-05-25 12:26:35 +0900

    Add comment.

--- debug.py
+++ debug.py
@@ -12,22 +12,22 @@ import mylibrary as mylib
 Security_code = "7203.T"
 
 # 価格のヒストリカルデータを取得し、csvファイルに保存
-# mine.stock_prices_to_csv(Security_code)
+# mylib.stock_prices_to_csv(Security_code)
 
 # 損益計算書をcsvファイルに保存
 # mylib.pl_to_csv(Security_code)
 
 # 貸借対照表をcsvファイルに保存
-# mine.balance_sheet_to_csv(Security_code)
+# mylib.balance_sheet_to_csv(Security_code)
 
 # キャッシュ・フロー計算書をcsvファイルに保存
-# mine.cash_flow_statement_to_csv(Security_code)
+# mylib.cash_flow_statement_to_csv(Security_code)
 
 # 銘柄のサマリーをcsvファイルに保存
-# mine.sammary_to_csv(Security_code)
+# mylib.sammary_to_csv(Security_code)
 
 # TOPIX500構成銘柄をcsvファイルに保存
-# mine.topix500_to_csv()
+# mylib.topix500_to_csv()
 
 # TOPIX500構成銘柄の証券コードを取得
 topix500_codes = mylib.get_codelist_topix500()

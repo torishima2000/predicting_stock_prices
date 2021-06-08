4b898befeea2482ab2c4518d0e3f66dc416c1b09
torishima2000 <ht19a055@oecu.jp>
2021-06-08 16:02:45 +0900

    Rename files

--- debug.py
+++ debug.py
@@ -37,19 +37,3 @@ import mylibrary as mylib
 
 # S&P500構成銘柄の証券コードを取得
 sandp500_codes = mylib.get_codelist_sandp500()
-
-# 実行済み
-# for s in topix500_codes:
-#     mylib.stock_prices_to_csv(str(s) + ".T")
-# mylib.stock_prices_to_csv("^N225")
-# for s in topix500_codes:
-#     mylib.pl_to_csv(str(s) + ".T")
-# for s in topix500_codes:
-#     mylib.balance_sheet_to_csv(str(s) + ".T")
-# for s in topix500_codes:
-#     mylib.cash_flow_statement_to_csv(str(s) + ".T")
-# for s in topix500_codes[150:]:
-#     mylib.sammary_to_csv(str(s) + ".T")
-
-bins = [(-0.5 + (i / 25.0)) for i in range(26)]
-print(bins)

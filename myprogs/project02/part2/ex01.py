729cdcc2f2000589f97d87ffb91ea2e4668b5a2e
torishima2000 <ht19a055@oecu.jp>
2021-09-14 19:31:53 +0900

    Rename project02/ex02 to project02/part2.

--- myprogs/project02/part2/ex01.py
+++ myprogs/project02/part2/ex01.py
@@ -21,6 +21,7 @@ end = datetime.datetime(*[2020, 1, 26])
 df = df[df.index >= begin]
 df = df[df.index <= end]
 
+
 # 移動平均線の算出
 df["SMA5"] = talib.SMA(np.array(df["Close"]), timeperiod=5)
 df["SMA25"] = talib.SMA(np.array(df["Close"]), timeperiod=25)
@@ -28,7 +29,7 @@ df["SMA50"] = talib.SMA(np.array(df["Close"]), timeperiod=50)
 df["SMA75"] = talib.SMA(np.array(df["Close"]), timeperiod=75)
 df["SMA100"] = talib.SMA(np.array(df["Close"]), timeperiod=100)
 
-# グラフの描画
+# 移動平均線の描画
 plt.figure(figsize=(10.24, 7.68))
 plt.plot(df["SMA5"], label="SMA5")
 plt.plot(df["SMA25"], label="SMA25")
@@ -39,3 +40,24 @@ plt.legend()
 plt.xlabel("date")
 plt.ylabel("price")
 plt.show()
+plt.close()
+
+
+# ボリンジャーバンドの算出
+df["upper1"], df["middle"], df["lower1"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=1, nbdevdn=1, matype=0)
+df["upper2"], df["middle"], df["lower2"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=2, nbdevdn=2, matype=0)
+df["upper3"], df["middle"], df["lower3"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=3, nbdevdn=3, matype=0)
+
+# ボリンジャーバンドの描画
+plt.figure(figsize=(10.24, 7.68))
+plt.plot(df["upper1"], label="upper1")
+plt.plot(df["lower1"], label="lower1")
+plt.plot(df["upper2"], label="upper2")
+plt.plot(df["lower2"], label="lower2")
+plt.plot(df["upper3"], label="upper3")
+plt.plot(df["lower3"], label="lower3")
+plt.legend()
+plt.xlabel("date")
+plt.ylabel("price")
+plt.show()
+plt.close()

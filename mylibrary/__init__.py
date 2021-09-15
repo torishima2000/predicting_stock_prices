2afa512d60352643ade2754d69750c091027b9c2
torishima2000 <ht19a055@oecu.jp>
2021-09-15 09:35:26 +0900

    Add a new module for plot figure to mylibrary\plot.py .

--- mylibrary/__init__.py
+++ mylibrary/__init__.py
@@ -1,2 +1,3 @@
 from mylibrary.data_to_csv import *
 from mylibrary.get_data import *
+from mylibrary.plot import *
---
+++ mylibrary/plot.py
@@ -0,0 +1,17 @@
+# グラフ描画を行うモジュール群
+
+# モジュールのインポート
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_chart(data, size={"x":10.24, "y":7.68}):
+    fig = plt.figure(figsize=(size["x"], size["y"]))
+    print(data)
+    for k, v in data.items():
+        plt.plot(v, label=k)
+    plt.legend()
+    plt.xlabel("date")
+    plt.ylabel("price")
+    plt.show()
+    plt.close()

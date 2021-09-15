1e1cc71785267f2783d7ef58677d613897fa1d19
torishima2000 <ht19a055@oecu.jp>
2021-09-15 09:12:56 +0900

    Add new source code to myprogs\project02\part02\ex02.py .

--- myprogs/project02/part02/ex02.py
+++ myprogs/project02/part02/ex02.py
@@ -9,35 +9,46 @@ import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import lightgbm as lgbm
+import lightgbm as lgb
 import talib
 import mylibrary as mylib
 
+def main():
+    # 株価データの取得
+    df = mylib.get_stock_prices("7203.T")
 
-# 株価データの取得
-df = mylib.get_stock_prices("7203.T")
+    # 元データの抽出
+    begin = datetime.datetime(*[2018, 5, 16])
+    end = datetime.datetime(*[2020, 1, 9])
+    df = df[df.index >= begin]
+    df = df[df.index <= end]
 
-# 元データの抽出
-begin = datetime.datetime(*[2018, 5, 16])
-end = datetime.datetime(*[2020, 1, 9])
-df = df[df.index >= begin]
-df = df[df.index <= end]
+    # 移動平均線の算出
+    df["SMA5"] = talib.SMA(np.array(df["Close"]), timeperiod=5)
+    df["SMA25"] = talib.SMA(np.array(df["Close"]), timeperiod=25)
+    df["SMA50"] = talib.SMA(np.array(df["Close"]), timeperiod=50)
+    df["SMA75"] = talib.SMA(np.array(df["Close"]), timeperiod=75)
+    df["SMA100"] = talib.SMA(np.array(df["Close"]), timeperiod=100)
 
-# 移動平均線の算出
-df["SMA5"] = talib.SMA(np.array(df["Close"]), timeperiod=5)
-df["SMA25"] = talib.SMA(np.array(df["Close"]), timeperiod=25)
-df["SMA50"] = talib.SMA(np.array(df["Close"]), timeperiod=50)
-df["SMA75"] = talib.SMA(np.array(df["Close"]), timeperiod=75)
-df["SMA100"] = talib.SMA(np.array(df["Close"]), timeperiod=100)
+    # ボリンジャーバンドの算出
+    df["upper1"], middle, df["lower1"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=1, nbdevdn=1, matype=0)
+    df["upper2"], middle, df["lower2"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=2, nbdevdn=2, matype=0)
+    df["upper3"], middle, df["lower3"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=3, nbdevdn=3, matype=0)
 
-# ボリンジャーバンドの算出
-df["upper1"], middle, df["lower1"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=1, nbdevdn=1, matype=0)
-df["upper2"], middle, df["lower2"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=2, nbdevdn=2, matype=0)
-df["upper3"], middle, df["lower3"] = talib.BBANDS(np.array(df["Close"]), timeperiod=25, nbdevup=3, nbdevdn=3, matype=0)
+    # MACDの算出
+    df["MACD"], df["MACDsignal"], df["MACDhist"] = talib.MACD(np.array(df["Close"]), fastperiod=12, slowperiod=26, signalperiod=9)
 
-# MACDの算出
-df["MACD"], df["MACDsignal"], df["MACDhist"] = talib.MACD(np.array(df["Close"]), fastperiod=12, slowperiod=26, signalperiod=9)
+    # RSIの算出
+    df["RSI9"] = talib.RSI(np.array(df["Close"]), timeperiod=9)
+    df["RSI14"] = talib.RSI(np.array(df["Close"]), timeperiod=14)
 
-# RSIの算出
-df["RSI9"] = talib.RSI(np.array(df["Close"]), timeperiod=9)
-df["RSI14"] = talib.RSI(np.array(df["Close"]), timeperiod=14)
+
+    train_data = lgb.Dataset(df)
+
+    # lightGBMによる予測
+    #model = lgb.LGBMClassifier(n_estimators=100, learing_rate=0.1, max_depth=-1)
+
+    print(train_data)
+
+if __name__=="__main__":
+    main()

5471cb7b1bf32364c414f15b6ebd35667e203d80
torishima2000 <ht19a055@oecu.jp>
2021-11-04 03:37:43 +0900

    Fix processing method .

--- mylibrary/talib.py
+++ mylibrary/talib.py
@@ -19,12 +19,12 @@ def vr_(close, volume, window=26):
         [DataFrame.Series]: 期間内の株価変わらずの日の出来高合計
     """
     df = pd.DataFrame()
-    df["up"] = np.where(np.append(np.nan, np.diff(close)) > 0, volume, 0)
-    df["down"] = np.where(np.append(np.nan, np.diff(close)) < 0, volume, 0)
-    df["same"] = np.where(np.append(np.nan, np.diff(close)) == 0, volume, 0)
-    u = df["up"].rolling(window=window, center=False).sum()
-    d = df["down"].rolling(window=window, center=False).sum()
-    s = df["same"].rolling(window=window, center=False).sum()
+    df.insert(len(df.columns), "up", np.where(np.append(np.nan, np.diff(close)) > 0, volume, 0))
+    df.insert(len(df.columns), "down", np.where(np.append(np.nan, np.diff(close)) < 0, volume, 0))
+    df.insert(len(df.columns), "same", np.where(np.append(np.nan, np.diff(close)) == 0, volume, 0))
+    u = df["up"].copy().rolling(window=window, center=False).sum()
+    d = df["down"].copy().rolling(window=window, center=False).sum()
+    s = df["same"].copy().rolling(window=window, center=False).sum()
     return u, d, s
 
 def vr_a(close, volume, window):
@@ -83,19 +83,19 @@ def colculate_feature(df, objective=None):
 
     # 特徴量の計算
     # 高値、安値、終値のnp.array化
-    high = np.array(df["High"])
-    low = np.array(df["Low"])
-    close = np.array(df["Close"])
-    volume = np.array(df["Volume"]).astype(np.float64)
+    high = np.array(df["High"].copy())
+    low = np.array(df["Low"].copy())
+    close = np.array(df["Close"].copy())
+    volume = np.array(df["Volume"].copy()).astype(np.float64)
 
     # 移動平均線の算出
-    df["SMA3"] = talib.SMA(close, timeperiod=3)
-    df["SMA5"] = talib.SMA(close, timeperiod=5)
-    df["SMA15"] = talib.SMA(close, timeperiod=15)
-    df["SMA25"] = talib.SMA(close, timeperiod=25)
-    df["SMA50"] = talib.SMA(close, timeperiod=50)
-    df["SMA75"] = talib.SMA(close, timeperiod=75)
-    df["SMA100"] = talib.SMA(close, timeperiod=100)
+    df.insert(len(df.columns), "SMA3", talib.SMA(close, timeperiod=3))
+    df.insert(len(df.columns), "SMA5", talib.SMA(close, timeperiod=5))
+    df.insert(len(df.columns), "SMA15", talib.SMA(close, timeperiod=15))
+    df.insert(len(df.columns), "SMA25", talib.SMA(close, timeperiod=25))
+    df.insert(len(df.columns), "SMA50", talib.SMA(close, timeperiod=50))
+    df.insert(len(df.columns), "SMA75", talib.SMA(close, timeperiod=75))
+    df.insert(len(df.columns), "SMA100", talib.SMA(close, timeperiod=100))
 
     # ボリンジャーバンドの算出
     df["upper1"], middle, df["lower1"] = talib.BBANDS(close, timeperiod=25, nbdevup=1, nbdevdn=1, matype=0)
@@ -106,37 +106,36 @@ def colculate_feature(df, objective=None):
     df["MACD"], df["MACDsignal"], df["MACDhist"] = talib.MACD(close, fastperiod=12, slowperiod=26, signalperiod=9)
 
     # RSIの算出
-    df["RSI9"] = talib.RSI(close, timeperiod=9)
-    df["RSI14"] = talib.RSI(close, timeperiod=14)
+    df.insert(len(df.columns), "RSI9", talib.RSI(close, timeperiod=9))
+    df.insert(len(df.columns), "RSI14", talib.RSI(close, timeperiod=14))
 
     # VR(Volume Ratio)の算出
-    df["VR"] = vr_a(close, volume, window=25)
+    df.insert(len(df.columns), "VR", vr_a(close, volume, window=25))
 
     # 移動平均乖離率(Moving Average Estrangement Rate)の算出
     sma15 = talib.SMA(close, timeperiod=15)
-    df["MAER15"] = 100 * (close - sma15) / sma15
+    df.insert(len(df.columns), "MAER15", (100 * (close - sma15) / sma15))
 
     # ADX(平均方向性指数)の算出
-    df["ADX"] = talib.ADX(high, low, close, timeperiod=14)
+    df.insert(len(df.columns), "ADX", talib.ADX(high, low, close, timeperiod=14))
 
     # CCI(商品チャンネル指数(Commodity Channel Index) )の算出
-    df["CCI"] = talib.CCI(high, low, close, timeperiod=14)
+    df.insert(len(df.columns), "CCI", talib.CCI(high, low, close, timeperiod=14))
 
     # ROC(rate of change)の算出
-    df["ROC"] = talib.ROC(close, timeperiod=10)
+    df.insert(len(df.columns), "ROC", talib.ROC(close, timeperiod=10))
 
     # ADOSC(チャイキンオシレーター:A/DのMACD)の算出
-    df["ADOSC"] = talib.ADOSC(high, low, close, volume, fastperiod=3, slowperiod=10)
+    df.insert(len(df.columns), "ADOSC", talib.ADOSC(high, low, close, volume, fastperiod=3, slowperiod=10))
 
     # ATR(Average True Range)の算出
-    df["ATR"] = talib.ATR(high, low, close, timeperiod=14)
+    df.insert(len(df.columns), "ATR", talib.ATR(high, low, close, timeperiod=14))
 
     # 目的変数の計算
     if (objective == "regression"):
-        df["target"] = (df["Open"].pct_change(-3).shift(-1) * -1)
-        df["target"] *= 100
+        df.insert(len(df.columns), "target", (100 * (df["Open"].pct_change(-3).shift(-1) * -1)))
     if (objective == "binary"):
-        df["growth rate"] = (df["Open"].pct_change(-3).shift(-1) * -1)
-        df["target"] = (df["growth rate"] > 0)
+        df.insert(len(df.columns), "growth rate", (df["Open"].pct_change(-3).shift(-1) * -1))
+        df.insert(len(df.columns), "target", (df["growth rate"].copy() > 0))
 
     return df
